513bfc65d2ff7f54f5354f033003e119851bd8ed
Amirreza <ghodrati.amirreza@gmail.com>
2023-04-23 21:30:19 +0200

    main.py updated

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import sqlite3
+import pandas as pd
 
 
 def main():
@@ -14,18 +15,22 @@ def main():
 
 def average():
     # average quantity of each car type
-    cursor.execute('''
-                SELECT dati.quantita_gasolio, dati2.avg_gasolio, mezzi.descrizione_mezzo, nominativi.nominativo 
-                FROM dati_rifornimento AS dati
-                JOIN mezzi ON dati.mezzo_id = mezzi.id
-                JOIN nominativi ON dati.nominativo_id = nominativi.id
-                JOIN (
-                    SELECT AVG(dati2.quantita_gasolio) AS avg_gasolio, dati2.mezzo_id FROM dati_rifornimento AS dati2
-                    WHERE dati2.quantita_gasolio > 0
-                    GROUP BY mezzo_id
-                ) AS dati2 ON dati.mezzo_id = dati2.mezzo_id
-                ORDER BY dati.quantita_gasolio DESC LIMIT 50;
-    ''')
+    data = pd.read_sql_query(
+        '''
+            SELECT dati.quantita_gasolio, dati2.avg_gasolio, mezzi.descrizione_mezzo, nominativi.nominativo 
+            FROM dati_rifornimento AS dati
+            JOIN mezzi ON dati.mezzo_id = mezzi.id
+            JOIN nominativi ON dati.nominativo_id = nominativi.id
+            JOIN (
+                SELECT AVG(dati2.quantita_gasolio) AS avg_gasolio, dati2.mezzo_id FROM dati_rifornimento AS dati2
+                WHERE dati2.quantita_gasolio > 0
+                GROUP BY mezzo_id
+            ) AS dati2 ON dati.mezzo_id = dati2.mezzo_id
+            ORDER BY dati.quantita_gasolio DESC LIMIT 50;
+        ''', conn)
+    df = pd.DataFrame(data).set_index('nominativo')
+    df.to_excel('output_files/output.xlsx')
+    print(df)
 
 
 main()
